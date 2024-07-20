2afa268deaec87eeaa2145b3f6db4ca330aa7395
hyprZona <92639283+hyprZona@users.noreply.github.com>
2024-07-20 19:55:49 +0530

    Delete books/scripts.js

--- books/scripts.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const navbarToggle = document.querySelector('.navbar-toggle');
-    const sidebar = document.querySelector('.sidebar');
-
-    navbarToggle.addEventListener('click', function() {
-        sidebar.classList.toggle('open');
-    });
-});
