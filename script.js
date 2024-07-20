617adc0fa2d71ddf1e6cecd4c80ae2022e34743a
hyprZona <92639283+hyprZona@users.noreply.github.com>
2024-07-20 15:34:36 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -103,6 +103,22 @@ $(document).ready(function(){
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2000
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
 });
